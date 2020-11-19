7c4abc53923da674682e2ac48dcaa02cb57538f2
mlazzarotto <mlazzarotto91@gmail.com>
2020-11-19 18:07:24 +0100

    Implemented function able to read ports nmap-style, ports validation, changed help function

--- main.py
+++ main.py
@@ -8,21 +8,18 @@ def cls(): return os.system('cls')
 
 
 def print_help():
-    print("You must provide 3 parameters:")
+    print("You must provide 2 parameters:")
     print("1) ip address or hostname")
-    print("2) start port")
-    print("3) end port")
+    print("2) ports to scan (20,21,22-25,80-100,443")
     print("Example: main.py scanme.nmap.org 1 1024")
     print("Argument given are:", sys.argv[1:])
     sys.exit()
 
 
-def scan_host(host, startPort, endPort):
+def scan_host(host, ports_to_scan):
     # list of port and relative service name
     openPorts = []
     socket.setdefaulttimeout(0.01)
-    startPort = int(startPort)
-    endPort = int(endPort)
 
     # trying to obtain the ip address
     try:
@@ -37,22 +34,13 @@ def scan_host(host, startPort, endPort):
     except:
         fqdn = host
 
-    # checking if startPort isn't bigger than endPort
-    if startPort > endPort:
-        print("Error: start port can't be bigger than end port!")
-        sys.exit()
-    # checking if the startPort and endPort are valid
-    if startPort < 0 or endPort > 65535:
-        print("Error: invalid port number!")
-        sys.exit()
-
+    
     print("Starting port scan of host: {}({})".format(host, fqdn))
-    print("Scanning all the ports between", startPort, "and", endPort)
-
+    
     # this is to get the execution time
     startTime = time.time()
 
-    for port in range(startPort, endPort+1):
+    for port in ports_to_scan:
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         # if the port is open
         if not sock.connect_ex((ip, port)):
@@ -68,19 +56,57 @@ def scan_host(host, startPort, endPort):
     print("Scan finished in {} seconds\n".format(executionTime))
     return openPorts
 
+# function to get a nice list of ports to scan from the terminal argument
+def inflatePortList(portlist_raw_string):
+    range_min_max = []
+    inflated_port_list = []
+
+    portlist_raw_list = portlist_raw_string.split(',')
+
+    # for every port number in the list
+    for port in portlist_raw_list:
+        if port != '':
+            # if the dash symbol is present, it's a range
+            if (port.find("-") != -1):
+                # adding the range of ports in a list
+                range_min_max.append(port.split('-'))
+            else:
+                # if the port doesn't contain letters
+                if port.isdigit():
+                    port = int(port)
+                    if port >= 0 and port <= 65535:
+                        inflated_port_list.append(port)
+
+    # for every range to create
+    for range_to_create in range_min_max:
+        min, max = int(range_to_create[0]), int(range_to_create[1])
+        
+        for port in range(min,max+1):
+            if port >= 0 and port <= 65535:
+                inflated_port_list.append(port)
+    
+    # remove duplicates and sort the list
+    inflated_port_list = sorted(set(inflated_port_list))
+
+    return(inflated_port_list)
 
 def main():
     # checking if the arguments are enough
-    if len(sys.argv) != 4:
+    if len(sys.argv) != 3:
         print_help()
 
     cls()
-    openPorts = scan_host(sys.argv[1], sys.argv[2], sys.argv[3])
-    print("Found {} ports open".format(len(openPorts)))
-    if len(openPorts) >= 1:
-        print("Port \t Service Name")
-        for port, serviceName in openPorts:
-            print(port, "\t", serviceName)
+
+    ports_to_scan = inflatePortList(sys.argv[2])
+    if len(ports_to_scan):
+        openPorts = scan_host(sys.argv[1], ports_to_scan)
+        print("Found {} ports open".format(len(openPorts)))
+        if len(openPorts) >= 1:
+            print("Port \t Service Name")
+            for port, serviceName in openPorts:
+                print(port, "\t", serviceName)
+        else:
+            print("No valid ports found")
 
 
 if __name__ == '__main__':
@@ -90,3 +116,5 @@ if __name__ == '__main__':
     except KeyboardInterrupt:
         print('Interrupted')
         os._exit(0)
+    except Exception as e:
+        print(e)
